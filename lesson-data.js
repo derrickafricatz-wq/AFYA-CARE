cf859ec3113d828c5bb05de855f2f93580935f5d
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-04-22 00:28:10 +0300

    Update lesson-data.js

--- lesson-data.js
+++ lesson-data.js
@@ -1,4 +1,4 @@
 let lessons={
-1:["https://i.imgur.com/i8MwdTh.mp4","https://i.imgur.com/Q0sidsk.mp4","https://i.imgur.com/7SajWhe.mp4"],
+1:["https://i.imgur.com/i8MwdTh.mp4","https://i.imgur.com/7SajWhe.mp4"],
 2:["https://i.imgur.com/nXmK9G4.mp4","https://i.imgur.com/AgYGVSb.mp4"],
 3:["https://i.imgur.com/Hb4qooS.mp4"]};
