72376e99ee5e85aca0df1362119add8af83b31ab
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-05-15 06:04:35 +0300

    Update lesson-data.js

--- lesson-data.js
+++ lesson-data.js
@@ -2,3 +2,4 @@ let lessons={
 1:["https://i.imgur.com/7SajWhe.mp4"],
 2:["https://i.imgur.com/nXmK9G4.mp4","https://i.imgur.com/AgYGVSb.mp4"],
 3:["https://i.imgur.com/Hb4qooS.mp4"]};
+10:["https://i.imgur.com/UEYYSmp.mp4"]};
