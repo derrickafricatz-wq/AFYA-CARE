407517ec3932eef24db84879436e490f1e615acc
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-05-15 06:16:43 +0300

    Fix lesson data structure and update lesson IDs

--- lesson-data.js
+++ lesson-data.js
@@ -1,5 +1,5 @@
 let lessons={
 1:["https://i.imgur.com/7SajWhe.mp4"],
 2:["https://i.imgur.com/nXmK9G4.mp4","https://i.imgur.com/AgYGVSb.mp4"],
-3:["https://i.imgur.com/Hb4qooS.mp4"]};
-0:["https://i.imgur.com/UEYYSmp.mp4"]};
+3:["https://i.imgur.com/Hb4qooS.mp4"],
+10:["https://i.imgur.com/UEYYSmp.mp4"]};
